f20492686424877da0dde034db6645c16213f760
Adam Spannbauer <SpannbauerAdam@gmail.com>
2023-08-29 09:23:04 -0400

    reuse func

--- bash-dictionaries/make-bash-dictionary-xl.py
+++ bash-dictionaries/make-bash-dictionary-xl.py
@@ -1,10 +1,17 @@
 import pandas as pd
 
-student_info_url = "https://docs.google.com/spreadsheets/d/1i9mObreN9__mEoRBTeEvUBsY4A8qpyeDtl-WqSvhzD8/export?format=csv"
-student_info = pd.read_csv(student_info_url)
-student_info = student_info[student_info["github_username"] != ("AdamSpannbauer")]
 
-student_names = student_info["first_name"] + student_info["last_name"]
+def read_student_names(
+    student_info_url="https://docs.google.com/spreadsheets/d/1i9mObreN9__mEoRBTeEvUBsY4A8qpyeDtl-WqSvhzD8/export?format=csv",
+):
+    student_info = pd.read_csv(student_info_url)
+    student_info = student_info[student_info["github_username"] != ("AdamSpannbauer")]
+
+    student_names = student_info["first_name"] + student_info["last_name"]
+    student_names = student_names.str.replace("'", "").tolist()
+
+    return student_names
+
 
 starter_dictionary = pd.DataFrame(
     [
@@ -31,6 +38,7 @@ starter_dictionary = pd.DataFrame(
     ]
 )
 
+student_names = read_student_names()
 with pd.ExcelWriter("bash-dictionaries.xlsx") as writer:
     for student_name in student_names:
         starter_dictionary.to_excel(writer, index=False, sheet_name=student_name)
